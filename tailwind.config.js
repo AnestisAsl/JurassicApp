242490935ce2c6b88c54860d2a101b5ab00bd627
Anestis Aslanidis <anestisaslanidis1997@gmail.com>
2022-06-09 22:39:20 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        MontserratAlternates: ["Montserrat Alternates"],
+        Montserrat: ["Montserrat"],
+      },
+    },
+  },
+  plugins: [],
+};
