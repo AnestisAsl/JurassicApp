518d026d80712d92ab17b58bd460683cf2411954
Anestis Aslanidis <anestisaslanidis1997@gmail.com>
2022-07-05 20:07:25 +0300

    Add files via upload

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import { SiFossilscm } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <div className="text-base bg-emerald-400 flex flex-row items-center justify-center">
+    <div className="font-MontserratAlternates text-2xl text-base bg-emerald-400 flex flex-row items-center justify-center">
+      <SiFossilscm className="mr-auto mx-4" size={32} />
       <h1 className="mx-4	">
         <a href="https://github.com/AnestisAsl">github</a>
       </h1>
@@ -10,7 +12,7 @@ const Footer = () => {
         <a href="https://github.com/AnestisAsl">Linkedin</a>
       </h1>
       <button className="mx-4	">
-        <Link href="/">HOME</Link>
+        <Link href="/">Back to home page</Link>
       </button>
     </div>
   );
