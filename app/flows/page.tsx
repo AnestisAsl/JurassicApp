63e2991d998316b176ee460ec7bb9db9f54925a1
Anestis Aslanidis <anestisaslanidis1997@gmail.com>
2024-11-22 17:23:27 +0200

    Add files via upload
    Importing the flow

--- app/flows/page.tsx
+++ app/flows/page.tsx
@@ -1,7 +1,9 @@
+import Flow from "@/customComponents/flows/flow";
+
 export default function Page() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-      <h1>FLOWS</h1>
+      <Flow />
     </div>
   );
 }
