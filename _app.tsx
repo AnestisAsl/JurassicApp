67da5ac99b2ab654d71ea7e1f1245eee91a3163f
Anestis Aslanidis <anestisaslanidis1997@gmail.com>
2024-11-23 20:27:08 +0200

    Delete _app.tsx

--- _app.tsx
+++
@@ -1,9 +0,0 @@
-import { AppProps } from "next/app";
-import { withApollo } from "../lib/apollo";
-import "../styles/globals.css";
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default withApollo(MyApp);
