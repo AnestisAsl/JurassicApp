a4f8030d1f31573692689e70a45ab34cf7c61490
Anestis Aslanidis <anestisaslanidis1997@gmail.com>
2024-11-25 17:36:07 +0200

    Add files via upload
    Added a new chart

---
+++ customComponents/charts/weightHorizontalBarChart.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { TrendingUp } from "lucide-react";
+import { Bar, BarChart, XAxis, YAxis } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+interface PropsObject {
+  name: string;
+  weight: number;
+  fill: string;
+}
+interface Props {
+  data: PropsObject[];
+}
+const chartConfig = {
+  weight: {
+    label: "weight",
+  },
+  Australotitan: {
+    label: "Australotitan",
+    color: "hsl(var(--chart-1))",
+  },
+} satisfies ChartConfig;
+
+export function WeightHorizontalBarChart(props: Props) {
+  console.log("weight data : ", props);
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Bar Chart - Mixed</CardTitle>
+        <CardDescription>January - June 2024</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <BarChart
+            accessibilityLayer
+            data={props.data}
+            layout="vertical"
+            margin={{
+              left: 0,
+            }}
+          >
+            <YAxis
+              dataKey="name"
+              type="category"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) =>
+                chartConfig[value as keyof typeof chartConfig]?.label
+              }
+            />
+            <XAxis dataKey="weight" type="number" hide />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Bar dataKey="weight" layout="vertical" radius={5} />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col items-start gap-2 text-sm">
+        <div className="flex gap-2 font-medium leading-none">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="leading-none text-muted-foreground">
+          Showing total visitors for the last 6 months
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
