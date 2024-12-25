f7be7da34cef4a490d639aaf814c5569ef3cf889
Anestis Aslanidis <anestisaslanidis1997@gmail.com>
2024-12-25 18:42:56 +0200

    Add files via upload
    Updated flow page

--- app/flows/page.tsx
+++ app/flows/page.tsx
@@ -1,9 +1,11 @@
-import Flow from "@/customComponents/flows/flow";
+import DinosaurClassificationFlow from "@/customComponents/flows/dinosaurClassificationFlow";
+import GeologicTimeFlow from "@/customComponents/flows/geologicTimeFlow";
 
 export default function Page() {
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-      <Flow />
+    <div className="grid place-items-center">
+      <DinosaurClassificationFlow />
+      <GeologicTimeFlow />
     </div>
   );
 }
