55c6afc8b90eb16255670a45e36f9189ba42bd98
Anestis Aslanidis <anestisaslanidis1997@gmail.com>
2025-04-02 16:48:23 +0300

    Add files via upload
    Changes to include tooltip in the Map component

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,9 +1,16 @@
+// @ts-nocheck
+
+"use client";
+
+import React, { useState } from "react";
 import FossilsMap from "@/customComponents/fossilsMap";
+import { Tooltip as ReactTooltip } from "react-tooltip";
 
 export default function Page() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
       <FossilsMap />
+      <ReactTooltip id="my-tooltip" float />
     </div>
   );
 }
